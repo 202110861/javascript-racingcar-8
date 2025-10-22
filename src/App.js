0c4c56045c06c34471cac6e3a04319911d0a8c6e
202110861 <dbsalsk2514@naver.com>
2025-10-22 14:21:09 +0900

    feat : 경주할 자동차 이름 분리 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import { Separate } from "./Separate.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    console.log(Separate(await carNames));
+  }
 }
 
 export default App;
---
+++ src/Separate.js
@@ -0,0 +1,5 @@
+export function Separate(names) {
+  const CAR_LIST = names.split(",");
+
+  return CAR_LIST;
+}
