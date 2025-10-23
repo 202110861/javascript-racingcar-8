2fa091399c5c3723ec61327aa6a7cb9500ba0b35
202110861 <dbsalsk2514@naver.com>
2025-10-23 20:26:40 +0900

    feat : 유효성 검사 함수 추가

---
+++ src/validator.js
@@ -0,0 +1,19 @@
+export function ValidateCarLength(carName) {
+  if (carName.length > 5) {
+    throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+  }
+}
+
+export function ValidateCarSeparator(carName) {
+  if (!carName.includes(",")) {
+    throw new Error("[ERROR] 자동차 이름 구분자는 쉼표(,)를 사용해야 합니다.");
+  }
+}
+
+export function ValidateCarNumber(count) {
+  if (!Number.isInteger(count)) {
+    throw new Error("[ERROR] 반복 횟수는 정수로 입력해야 합니다.");
+  } else if (isNaN(count)) {
+    throw new Error("[ERROR] 반복 횟수는 숫자로 입력해야 합니다.");
+  }
+}
