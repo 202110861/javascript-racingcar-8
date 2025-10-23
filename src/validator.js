6f51287aeb78d2199a106cac973bd114865f9fa1
202110861 <dbsalsk2514@naver.com>
2025-10-23 20:49:53 +0900

    refactor : 유효성 검사 함수 로직 수정

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,8 @@
 export function ValidateCarLength(carName) {
   if (carName.length > 5) {
     throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+  } else if (carName.length === 0) {
+    throw new Error("[ERROR] 자동차 이름은 빈 값이 될 수 없습니다.");
   }
 }
 
@@ -11,9 +13,11 @@ export function ValidateCarSeparator(carName) {
 }
 
 export function ValidateCarNumber(count) {
-  if (!Number.isInteger(count)) {
-    throw new Error("[ERROR] 반복 횟수는 정수로 입력해야 합니다.");
-  } else if (isNaN(count)) {
+  if (count <= 0) {
+    throw new Error("[ERROR] 반복 횟수는 1 이상으로 입력해야 합니다.");
+  } else if (isNaN(Number(count))) {
     throw new Error("[ERROR] 반복 횟수는 숫자로 입력해야 합니다.");
+  } else if (!Number.isInteger(Number(count))) {
+    throw new Error("[ERROR] 반복 횟수는 정수로 입력해야 합니다.");
   }
 }
